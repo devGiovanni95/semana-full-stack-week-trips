d1a4381aa8ab408bb0a0620fbbd6a6259bf67e89
devGiovanni95 <giovanni.devjava@hotmail.com>
2023-07-05 21:49:52 -0300

    feat: add next auth provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import Image from 'next/image'
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+"use client";
+import { signIn } from "next-auth/react";
 
 export default function Home() {
+
+ 
   return (
-    <h1>Home Page</h1>
-  )
+    <div>
+      <button onClick={() => signIn()}>
+        Login
+      </button>
+
+    </div>
+  );
 }
